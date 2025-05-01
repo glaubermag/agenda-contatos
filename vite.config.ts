2ba1c92b7719cc2b97fe9a36b73a0ee7effcea08
Glauber Ariel Magalhães <29615342+glaubermag@users.noreply.github.com>
2025-05-01 11:03:25 -0300

    fix: ajusta base URL para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/agenda-contatos/' : '/'
+  base: '/agenda-contatos/'
 })
