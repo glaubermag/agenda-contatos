0dc2b17a2e0673ab8750c6605aef92b868a21e5e
Glauber Ariel Magalhães <29615342+glaubermag@users.noreply.github.com>
2025-05-01 21:05:13 -0300

    feat: usando api pública feita em php

---
+++ src/config/api.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'https://glaubermag.dev.br/test/agenda/api.php'
--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -2,7 +2,7 @@ import type { Contact } from '../types/contact'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://glaubermag.dev.br/test/agenda/api.php',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -10,26 +10,26 @@ const api = axios.create({
 
 export const contactService = {
   async getAll(): Promise<Contact[]> {
-    const { data } = await api.get<Contact[]>('/users')
+    const { data } = await api.get<Contact[]>('/contacts')
     return data
   },
 
   async getById(id: number): Promise<Contact> {
-    const { data } = await api.get<Contact>(`/users/${id}`)
+    const { data } = await api.get<Contact>(`/contacts/${id}`)
     return data
   },
 
   async create(contact: Omit<Contact, 'id'>): Promise<Contact> {
-    const { data } = await api.post<Contact>('/users', contact)
+    const { data } = await api.post<Contact>('/contacts', contact)
     return data
   },
 
   async update(id: number, contact: Partial<Contact>): Promise<Contact> {
-    const { data } = await api.patch<Contact>(`/users/${id}`, contact)
+    const { data } = await api.patch<Contact>(`/contacts/${id}`, contact)
     return data
   },
 
   async delete(id: number): Promise<void> {
-    await api.delete(`/users/${id}`)
+    await api.delete(`/contacts/${id}`)
   }
 } 
